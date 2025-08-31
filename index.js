3a995652a9c5a0f44d778ff49dc969f74fe875e4
Chitranjan kumar <chitranjan75075@gmail.com>
2025-08-31 21:18:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+let student ={ 
+    name:"chitranjan",
+    age: 20,
+    gender : "male"
+}
+console.log(student.name)
+console.log(student.age)
+console.log(student.gender)
+
+
+
+
+
+// spread operator
+
+let str = "aman";
+console.log(...str)
+
+
+let obj = {
+    name: "Ansh",
+    age:50
+}
+let obj1={
+    ...obj,
+    ...obj1,
+    gender:"male"
+}
+
+console.log(person)
